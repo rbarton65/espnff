3eabb6083b82a4dfb9584204b07e9c8b16ef860b
Rich Barton <rbarton@corsis.com>
2016-05-13 18:52:44 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name = 'espnff',
+    
+    packages = ['espnff'],
+    
+    version = '0.1.1',
+    
+    description = 'ESPN fantasy football power rankings algorithm',
+    
+    author = 'Rich Barton',
+    
+    author_email = 'rbart65@gmail.com',
+    
+    packages = ['lxml', 'requests', 'json'],
+    
+    classifiers = [
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
+)
