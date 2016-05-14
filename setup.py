41188cfd876fd56c5b0e9b74bfb58870f63fc85f
Rich Barton <rbarton@corsis.com>
2016-05-14 01:36:10 -0400

    pep8

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 from setuptools import setup
 
 setup(
-    name = 'espnff',
-    
-    packages = ['espnff'],
-    
-    version = '0.1.1',
-    
-    description = 'ESPN fantasy football power rankings algorithm',
-    
-    author = 'Rich Barton',
-    
-    author_email = 'rbart65@gmail.com',
-    
-    install_requires = ['lxml', 'requests', 'numpy'],
-    
-    url = 'https://github.com/rbarton65/espnff',
-    
-    classifiers = [
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    name='espnff',
 
+    packages=['espnff'],
+
+    version='0.1.1',
+
+    description='ESPN fantasy football power rankings algorithm',
+
+    author='Rich Barton',
+
+    author_email='rbart65@gmail.com',
+
+    install_requires=['lxml', 'requests', 'numpy'],
+
+    url='https://github.com/rbarton65/espnff',
+
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
