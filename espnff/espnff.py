04953317d3d54638bef2cbc73d26f55a69e423bd
rbarton65 <rbart65@gmail.com>
2016-10-04 23:35:13 -0400

    replacing exisitng code with new url

---
+++ espnff/espnff.py
@@ -0,0 +1,65 @@
+import requests
+
+
+class League(object):
+    '''Creates a League instance for Public ESPN league'''
+    def __init__(self, league_id, year):
+        self.league_id = league_id
+        self.year = year
+        self.ENDPOINT = "http://games.espn.com/ffl/api/v2/"
+        self.members = []
+        self._fetch_members()
+
+    def _fetch_members(self):
+        '''Fetch members in league'''
+        url = "%sleagueSettings?leagueId=%s&seasonId=%s"
+        r = requests.get(url % (self.ENDPOINT, self.league_id, self.year))
+        data = r.json()
+        teams = data['leaguesettings']['teams']
+
+        for team in teams:
+            self.members.append(Member(teams[team]))
+
+        # replace opponentIds in schedule with member instances
+        for member in self.members:
+            for week, matchup in enumerate(member.schedule):
+                for opponent in self.members:
+                    if matchup == opponent.teamId:
+                        member.schedule[week] = opponent
+
+
+class Member(object):
+    '''Members are part of the league'''
+    def __init__(self, data):
+        self.teamId = data['teamId']
+        self.teamAbbrev = data['teamAbbrev']
+        self.teamName = "%s %s" % (data['teamLocation'], data['teamNickname'])
+        self.divisionId = data['division']['divisionId']
+        self.divisionName = data['division']['divisionName']
+        self.wins = data['record']['overallWins']
+        self.losses = data['record']['overallLosses']
+        self.pointsFor = data['record']['pointsFor']
+        self.pointsAgainst = data['record']['pointsAgainst']
+        self.owner = "%s %s" % (data['owners'][0]['firstName'], data['owners'][0]['lastName'])
+        self.schedule = []
+        self.scores = []
+        self._fetch_schedule(data)
+
+    def _fetch_schedule(self, data):
+        '''Fetch schedule and scores for team'''
+        matchups = data['scheduleItems']
+
+        for matchup in matchups:
+            if matchup['matchups'][0]['isBye'] == False:
+                if matchup['matchups'][0]['awayTeamId'] == self.teamId:
+                    score = matchup['matchups'][0]['awayTeamScores'][0]
+                    opponentId = matchup['matchups'][0]['homeTeamId']
+                else:
+                    score = matchup['matchups'][0]['homeTeamScores'][0]
+                    opponentId = matchup['matchups'][0]['awayTeamId']
+            else:
+                score = matchup['matchups'][0]['homeTeamScores'][0]
+                opponentId = matchup['matchups'][0]['homeTeamId']
+                
+            self.scores.append(score)
+            self.schedule.append(opponentId)
