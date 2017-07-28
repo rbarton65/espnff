badef595bf2e7b5bf28d649b6cc4bd71ba227a19
Rich Barton <rbart65@gmail.com>
2017-07-27 20:32:14 -0400

    changed espnff.py to league.py to reorganize package

--- espnff/__init__.py
+++ espnff/__init__.py
@@ -8,7 +8,7 @@ __all__ = ['League',
            'UnknownLeagueException'
            ]
 
-from .espnff import League
+from .league import League
 from .team import Team
 from .settings import Settings
 from .matchup import Matchup
